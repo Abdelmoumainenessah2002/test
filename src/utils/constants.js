1f6a5d3ec57c1a833ffcddb28bddb6613df05238
Abdelmoumaine2002 <Abdelmoumainenessah@gmail.com>
2025-11-03 01:04:26 +0100

    nessah

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const request = axios.create({
-  baseURL: "https://9039ad245429.ngrok-free.app/api/auth/register",
+  baseURL: "https://9039ad245429.ngrok-free.app",
 });
 
 export default request;
